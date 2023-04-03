ee7966343efa34cc94b0b4b80b785657ce8ec679
EndiMimini <3ndi.mimini@gmail.com>
2023-04-03 18:48:38 +0200

    fshiva disa komente

--- fundamentals2.py
+++ fundamentals2.py
@@ -103,25 +103,6 @@ z = [ {'x': 10, 'y': 20} ]
 z[0]['y'] = 30
 
 
-# 7 LOCATIONS
-# San Jose
-# Seattle
-# Dallas
-# Chicago
-# Tulsa
-# DC
-# Burbank
-    
-# 8 INSTRUCTORS
-# Michael
-# Amy
-# Eduardo
-# Josh
-# Graham
-# Patrick
-# Minh
-# Devon
-
 dojo = {
    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
